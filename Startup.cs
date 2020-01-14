af725cd035e0ed62905913243d6df595565852a5
nealhartley <neal.w.hartley@gmail.com>
2020-01-14 15:24:22 +1300

    pushing for workflow

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace leashApi
         }
 
         public IConfiguration Configuration { get; }
-
+        // touched
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
